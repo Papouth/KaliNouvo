c168bc1d197983f070221c146e22c73c560322d6
dupas louis <tawairpofficiel@gmail.com>
2023-05-29 15:12:53 +0200

    Tp zone 1
    tp zone 1 to hub

---
+++ Assets/Scripts/CustomsEvents/TeleporteurZone.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleporteurZone : CustomsTriggers
+{
+    #region Variables
+    [Header("TP Attributes")]
+    [Tooltip("Les temporalités de la scène dans laquelle on va se tp")]
+    [SerializeField] private string scenePast;
+    [SerializeField] private string scenePresent;
+    private bool inTrigger;
+    private bool haveInteract;
+    [Tooltip("L'endroit où kali va se tp")]
+    [SerializeField] private Transform tp;
+
+    [Header("Player Components")]
+    private PlayerTemporel playerTempo;
+    private CharacterController cc;
+
+    [Header("Black Screen")]
+    public float durationRenderFeature;
+    [Tooltip("Set 0 pour empecher le matériaux de revenir")]
+    public float speedFeatureMat = 1;
+    public Blit renderFeatureMat;
+    public string nameParameterFeature;
+    #endregion
+
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inTrigger = true;
+
+            playerTempo = other.GetComponent<PlayerTemporel>();
+            cc = other.GetComponent<CharacterController>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) inTrigger = false;
+    }
+
+    public override void Interact()
+    {
+        if (inTrigger && !haveInteract)
+        {
+            // On empêche le joueur de re-interargir avec
+            haveInteract = true;
+
+            // On désactive le character controller
+            cc.enabled = false;
+
+            // Fondu en noir
+            StartCoroutine(SetBlackScreen());
+        }
+    }
+
+    private void LoadTempo()
+    {
+        // On décharge les scènes de la zone
+        SceneManager.UnloadSceneAsync(playerTempo.past);
+        SceneManager.UnloadSceneAsync(playerTempo.present);
+
+        // On change les scènes passé et présent du joueur
+        playerTempo.past = scenePast;
+        playerTempo.scenesToLoad = playerTempo.past;
+        playerTempo.present = scenePresent;
+        playerTempo.scenesToUnload = playerTempo.present;
+
+        // On load les scènes passé et présent de la nouvelle zone
+        SceneManager.LoadScene(playerTempo.present, LoadSceneMode.Additive);
+        SceneManager.LoadScene(playerTempo.past, LoadSceneMode.Additive);
+
+        // Reset des scènes pour le joueur
+        playerTempo.scenesToLoad = playerTempo.present;
+        playerTempo.scenesToUnload = playerTempo.past;
+
+        // On met Kali a la bonne position
+        cc.transform.position = tp.position;
+    }
+
+    public void StartBlackScreen()
+    {
+        StartCoroutine(SetBlackScreen());
+    }
+
+    public IEnumerator SetBlackScreen()
+    {
+        float i = 0;
+
+        while (i < 1.3f)
+        {
+            float currentFloat = renderFeatureMat.settings.blitMaterial.GetFloat(nameParameterFeature);
+            currentFloat = Mathf.Lerp(currentFloat, 1.3f, i);
+            renderFeatureMat.settings.blitMaterial.SetFloat(nameParameterFeature, currentFloat);
+
+            i = i + Time.deltaTime / speedFeatureMat;
+
+            yield return new WaitForSeconds(Time.deltaTime / speedFeatureMat);
+        }
+
+        if (durationRenderFeature == 0) StopAllCoroutines();
+
+        LoadTempo();
+
+        yield return new WaitForSeconds(durationRenderFeature);
+
+        // On réactive le character controller
+        cc.enabled = true;
+
+        float j = 1.3f;
+
+        while (j > 0)
+        {
+            float currentFloat = renderFeatureMat.settings.blitMaterial.GetFloat(nameParameterFeature);
+            currentFloat = Mathf.Lerp(-0.1f, currentFloat, j);
+
+            renderFeatureMat.settings.blitMaterial.SetFloat(nameParameterFeature, currentFloat);
+
+            j = j - Time.deltaTime / speedFeatureMat;
+
+            yield return new WaitForSeconds(Time.deltaTime / speedFeatureMat);
+        }
+
+
+        renderFeatureMat.settings.blitMaterial.SetFloat(nameParameterFeature, -.1f);
+    }
+
+    private void OnDisable()
+    {
+        renderFeatureMat.settings.blitMaterial.SetFloat(nameParameterFeature, -.1f);
+        StopAllCoroutines();
+    }
+}
